c20ff3fba667c6fe91c707bba7b2756e74aacc33
vayne <miguel.osorio19@yahoo.com>
2021-12-12 16:21:50 -0600

    adding local storage, setting up retrieval of said local storage upon refresh - work in progress

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -2,3 +2,80 @@
 const today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 console.log(today.toString());
+
+// console logging the time in military time
+console.log(moment().hour());
+
+// variable for the container holding the timeblocks - called the class in the div
+var containerEl = $(".container")
+
+// time variables
+var timeEl = 
+[   
+    "9:00 AM", 
+    "10:00 AM", 
+    "11:00 AM", 
+    "12:00 PM",
+    "1:00 PM", 
+    "2:00 PM",
+    "3:00 PM", 
+    "4:00 PM", 
+    "5:00 PM"
+];
+
+
+// setting up creating a task function();
+var createTask = function() {
+    
+    var t = 0;
+    var currentHour = moment().hour();
+    containerEl.empty()
+    // ${timeEl[t++]} calls the Time in the array - timeEl
+    for (let i = 9; i < 18; i++) {
+        var status = "";// declaring an empty status variable first
+        
+        // status is set up as a variable and later called in the dynamic HTML ${status}
+        // redeclaring the variable with updated values in the for loop
+        // status value is the class
+        if (i === currentHour) {
+            status = "present";
+        }
+
+        if (i < currentHour) {
+            status = "past";
+        }
+
+        if (i > currentHour) {
+            status = "future";
+        }
+        
+        // textarea id is relating to button data-id ${i} on click event
+        containerEl.append(`
+        <div id="9a" class="row time-block">
+        <div class="col-1 hour">${timeEl[t++]}</div> 
+       
+         <textarea id="${i}" class="col-10 description ${status}"></textarea>
+       
+        <button data-id="${i}" class="col-1 saveBtn d-flex justify-content-center align-items-center">
+         <i class="fas fa-lock"> SAVE</i>
+        </button>
+        </div>
+        `)
+    };
+
+    var saveBtnEl = $(".saveBtn");
+    saveBtnEl.on("click", function() {
+        var id = $(this).attr("data-id"); // Select 'this' with an attribute of data-id which is the button
+
+        var event = $("#"+ id).val();
+        // console.log(event);
+        localStorage.setItem(id, JSON.stringify(event));
+        console.log(id);
+        let x = $(".description")
+        x.value = localStorage.getItem(id);
+        console.log(x);
+    });
+}; 
+
+
+createTask();
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ SO THAT I can manage my time effectively
 # Acceptance Criteria
 * GIVEN I am using a daily planner to create a schedule
 1. WHEN I open the planner
- - THEN the current day is displayed at the top of the calendar
+ - THEN the current day is displayed at the top of the calendar 
 2. WHEN I scroll down
  - THEN I am presented with time blocks for standard business hours
 3. WHEN I view the time blocks for that day
@@ -19,3 +19,21 @@ SO THAT I can manage my time effectively
  - THEN the text for that event is saved in local storage
 6. WHEN I refresh the page
  - THEN the saved events persist
+
+# TODO's
+1. Current day displayed at the header ✔️
+    - src moment.js library
+2. Time blocks should show on the body that reflect 9-5 working hrs ✔️
+    - Time blocks: ✔️
+        - color coded [past, present, future]
+        - classes already set in styles.css
+        - clickable, once clicked user can enter a text/event
+    - save button:✔️
+        - function, events are saved in a "LOCAL STORAGE"
+3. Upon refresh, events saved should persist 
+
+## Contributors
+
+## Description
+
+## GIF OR JPEG 
